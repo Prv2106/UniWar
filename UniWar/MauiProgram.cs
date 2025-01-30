c800e4e30f615e13b4eb8fe9e6fa97f07b2b4a3d
Giuseppe Leocata <peppeleocata@gmail.com>
2025-01-30 11:15:33 +0100

    cambiati i colori dell'app e rimossa la dark mode

--- UniWar/MauiProgram.cs
+++ UniWar/MauiProgram.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
 
-namespace UniWar
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
+
+namespace UniWar {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -13,7 +13,19 @@ namespace UniWar
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .ConfigureLifecycleEvents(events => {
+				events.AddWindows(window => {
+					window.OnWindowCreated(w => {
+						var appWindow = w.GetAppWindow(); 
+						if (appWindow != null) {
+							int width = 650;  // Larghezza finestra
+							int height = 1200; // Altezza finestra
+							appWindow.Resize(new Windows.Graphics.SizeInt32(width, height));							
+						}
+					});
+				});
+			});
 
 #if DEBUG
     		builder.Logging.AddDebug();
---
+++ UniWar/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+using UniWar.views;
+
+namespace UniWar {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+            // metodo che, durante la compilazione, collega la logica C# agli elementi XAML 
+        }
+
+        private async void OnNuovaPartitaButtonClicked(object sender, EventArgs e) {
+            // PushAsync: metodo della proprietà Navigation per navigare verso la pagina Integration e aggiungerla alla stack di navigazione.
+            // N.B: await è usato perché PushAsync è un metodo asincrono.
+            await Navigation.PushAsync(new Integration());
+        }
+    }
+}
